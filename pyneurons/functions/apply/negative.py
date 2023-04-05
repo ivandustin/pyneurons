e228113dd8998ecbdf9a98c5875d04560cc544a7
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-04-05 20:22:10 +0800

    Add jit to apply functions

--- pyneurons/functions/apply/negative.py
+++ pyneurons/functions/apply/negative.py
@@ -1,6 +1,8 @@
 from jax.numpy import negative as negative_function
 from .positive import positive
+from jax import jit
 
 
+@jit
 def negative(matrix, x):
     return negative_function(positive(matrix, x))
--- pyneurons/functions/apply/positive.py
+++ pyneurons/functions/apply/positive.py
@@ -1,6 +1,8 @@
 from jax.numpy import matmul
 from pyneurons.functions import activation
+from jax import jit
 
 
+@jit
 def positive(matrix, x):
     return activation(matmul(x, matrix))
