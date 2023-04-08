ae3ca3594ef5c6f7bee1531af18636e610dfc2f7
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-04-08 11:56:27 +0800

    Export classes and functions

--- pyneurons/__init__.py
+++ pyneurons/__init__.py
@@ -1,1 +1,6 @@
-#
+from .classes import Excitatory, Inhibitory
+from .functions import concat, stack
+from . import functions
+from . import classes
+
+__all__ = ["Excitatory", "Inhibitory", "concat", "stack", "functions", "classes"]
--- pyneurons/functions/__init__.py
+++ pyneurons/functions/__init__.py
@@ -2,5 +2,7 @@ from .activation import activation
 from .synapses import synapses
 from .concat import concat
 from .stack import stack
+from . import apply
+from . import core
 
-__all__ = ["activation", "synapses", "concat", "stack"]
+__all__ = ["activation", "synapses", "concat", "stack", "apply", "core"]
