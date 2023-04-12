a6c24c50bafe002e9a36a18674724ef14a5f9948
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-04-12 19:37:08 +0800

    Fix flake8 issues

--- pyneurons/__init__.py
+++ pyneurons/__init__.py
@@ -1,2 +1,13 @@
-from .functions import *
-from .classes import *
+from .functions import concat, stack
+from .classes import Neuron, Binary
+from . import functions
+from . import classes
+
+__all__ = [
+    "functions",
+    "classes",
+    "Binary",
+    "Neuron",
+    "concat",
+    "stack",
+]
