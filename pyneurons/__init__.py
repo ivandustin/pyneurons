9262df087ec89eb8d40851ad9d3821099ed5c851
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-04-12 23:36:28 +0800

    Export pytree functions

--- pyneurons/__init__.py
+++ pyneurons/__init__.py
@@ -1,5 +1,5 @@
+from .functions.pytrees import map, reduce, concat, stack, split, unstack
 from .functions.factories.models import subclass
-from .functions.pytrees import concat, stack
 from .classes import Neuron, Binary
 from . import functions
 from . import classes
@@ -8,8 +8,12 @@ __all__ = [
     "functions",
     "subclass",
     "classes",
+    "unstack",
     "Binary",
     "Neuron",
     "concat",
+    "reduce",
+    "split",
     "stack",
+    "map",
 ]
