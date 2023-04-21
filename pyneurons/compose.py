8206b50896b5650e8d609afefe29bfba3ca7501b
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-04-21 16:39:47 +0800

    Pre jit function in compose

--- pyneurons/compose.py
+++ pyneurons/compose.py
@@ -2,7 +2,9 @@ from jax import jit
 
 
 def compose(name, cls, function):
+    function = jit(function)
+
     def __call__(self, x):
-        return jit(function)(cls.__call__(self, x))
+        return function(cls.__call__(self, x))
 
     return type(name, (cls,), {"__call__": __call__})
